ca2d224f8818b738ed895db59a0adf08fae01a3d
synfonaut <synfonaut@protonmail.com>
2019-11-18 22:58:30 -0800

    cleanup cli

--- cli.js
+++ cli.js
@@ -1,34 +1,60 @@
 #!/usr/bin/env node --require esm
 
+const readline = require("readline");
+
+import config from "./config"
 import { fire } from "./fire"
 import { split } from "./split"
 import { utxos } from "./utxos"
 import { B2P2PBackend } from "./backends"
 
 if (require.main == module) {
-    const wif = "L4Wcacd6Xy1rNEGG3dpThzhZ4rdESaGaaLDoPCboLbAyvv87h4Hf";
-
     if (process.argv[2] == "split") {
-        const num = 25;
         const satoshis = 1000;
 
-        split(wif, num, satoshis).catch(e => {
+        split(config.wif, 25, satoshis).catch(e => {
             console.log(`ERROR while splitting utxos ${e.message}`);
         });
     } else if (process.argv[2] == "fire") {
-        const num = 25;
-        const satoshis = 800;
-        const target = "1KbiAScTy2fAeVp1rAs1e7LPCtvTgUqNMz"; // xanadu@simply.cash
 
-        const backend = new B2P2PBackend();
-        backend.ready = function() {
-            console.log("backend ready");
-            fire(wif, num, satoshis, target, backend).catch(e => {
-                console.log(`ERROR while firing transactions ${e.message}`);
-            });
-        };
+        const satoshis = 800;
+        const target = process.argv[3];
+        if (!target) {
+            console.log(`ERROR invalid target`);
+            process.exit();
+        }
+
+        const num = (process.argv[4] ? Number(process.argv[4]) : 10);
+
+        const prompt  = readline.createInterface(process.stdin, process.stdout);
+        console.log("===================================================");
+        console.log("⚠️  WARNING ⚠️");
+        console.log(`\nAre you sure you want to FIRE:`);
+        console.log(`number: ${num}`);
+        console.log(`satoshis: ${satoshis}`);
+        console.log(`target: ${target}`);
+        console.log("⚠️  WARNING ⚠️");
+        console.log("===================================================");
+        prompt.setPrompt("Are you sure you want to proceed? [y/N]> ");
+        prompt.prompt();
+        prompt.on("line", async function(line) {
+            if (line == "y") {
+                console.log("🔫 FIRE AT WILL");
+                const backend = new B2P2PBackend();
+                backend.ready = function() {
+                    console.log("backend ready");
+                    fire(config.wif, num, satoshis, target, backend).catch(e => {
+                        console.log(`ERROR while firing transactions ${e.message}`);
+                    });
+                };
+            } else {
+                console.log("\nSkipping... CEASE FIRE\n");
+            }
+            prompt.close();
+        });
+
     } else if (process.argv[2] == "utxos") {
-        utxos(wif).then(results => {
+        utxos(config.wif).then(results => {
             for (const utxo of results) {
                 console.log(utxo.satoshis, `${utxo.txid}:${utxo.vout}`);
             }
